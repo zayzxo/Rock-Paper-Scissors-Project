4b303a30805744617fcb840b3518e526689eb39b
zayzxo <isaiahcarter154@gmail.com>
2022-07-29 21:51:14 -0400

    Added every scenario in a Rock Paper Scissors Game

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-let answers = ["rock", "paper", "scissors"];
+let answers = ["rock", "paper", "scissor"];
 
 function getComputerChoice(answer) {
   (randomNumber = Math.floor(Math.random() * answers.length)),
     (pcChoice = answers[randomNumber]);
   return pcChoice;
 }
-const playerChoice = "rock";
+const playerChoice = prompt("Rock, Paper, Scissor?");
 const computerChoice = getComputerChoice();
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
     console.log("It is a TIE!");
+  } else if (playerChoice == "rock") {
+    if (computerChoice == "paper") console.log("Computer Wins");
+  } else if (playerChoice == "paper") {
+    if (computerChoice == "scissor") console.log("Computer Wins");
+  } else if (playerChoice == "scissor") {
+    if (computerChoice == "rock") console.log("Computer Wins");
+    else {
+      console.log("YOU WIN");
+    }
   }
 }
-console.log(playRound(playerChoice, computerChoice));
-//function playerSelection(playerAnswer) {
-//playerAnswer = prompt("Rock, Paper, Scissors?");/
+//console.log(playRound(playerChoice, computerChoice));
