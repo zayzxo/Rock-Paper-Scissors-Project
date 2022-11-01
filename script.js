933656e79958f5644c8e78e1cde252d9ff14afc9
zayzxo <109442370+zayzxo@users.noreply.github.com>
2022-10-31 23:33:50 -0400

    test

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 let answers = ["rock", "paper", "scissor"];
 
-function getComputerChoice(answer) {
-  (randomNumber = Math.floor(Math.random() * answers.length)),
-    (pcChoice = answers[randomNumber]);
-  return pcChoice;
-}
 const playerChoice = prompt("Rock, Paper, Scissor?");
-const computerChoice = getComputerChoice();
+const computerChoice = "rock";
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    console.log("It is a TIE!");
-  } else if (playerChoice == "rock") {
-    if (computerChoice == "paper") console.log("Computer Wins");
-  } else if (playerChoice == "paper") {
-    if (computerChoice == "scissor") console.log("Computer Wins");
-  } else if (playerChoice == "scissor") {
-    if (computerChoice == "rock") console.log("Computer Wins");
-    else {
-      console.log("YOU WIN");
-    }
+    return "It is a TIE!";
+  } else if (playerChoice == "rock" && computerChoice == "paper") {
+    return "Computer Wins";
+  } else if (playerChoice == "paper" && computerChoice == "scissor") {
+    return "Computer Wins";
+  } else if (playerChoice == "scissor" && computerChoice == "rock") {
+    return "Computer Wins";
+  } else {
+    return "YOU WIN";
   }
 }
-//console.log(playRound(playerChoice, computerChoice));
+
+console.log(playRound(playerChoice, computerChoice));
